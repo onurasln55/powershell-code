376511c42facc763a3d959b0c5156dd500a75641
Muhammet Onur <onurasln55@gmail.com>
2024-11-29 14:53:38 +0300

    Update komutlar.ps1

--- komutlar.ps1
+++ komutlar.ps1
@@ -1,5 +1,3 @@
-Get-ChildItem -Path C:\ -Recurse -Filter "dosyaadi.txt" -ErrorAction SilentlyContinue # dosya arama
-
 Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" # autorun da neler var
 
 Get-Process | Sort-Object CPU -Descending | Select-Object -First 10 #şuan çalışan neler var
